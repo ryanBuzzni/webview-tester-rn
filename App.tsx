762169c6e3d55c0ec3146fd5fb0a94ca7b5720f5
ryan <ryan@buzzni.com>
2024-09-26 16:30:26 +0900

    🎨 웹뷰 테스트용 앱 개발

--- App.tsx
+++ App.tsx
@@ -1,118 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useEffect} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {PERMISSIONS, request} from 'react-native-permissions';
+import WebView from 'react-native-webview';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function WebviewScreen() {
+  useEffect(() => {
+    request(PERMISSIONS.IOS.SPEECH_RECOGNITION).then(result => {
+      // Alert.alert(result);
+      console.log(result);
+    });
+    request(PERMISSIONS.IOS.MICROPHONE).then(result => {
+      // Alert.alert(result);
+      console.log(result);
+    });
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    request(PERMISSIONS.ANDROID.RECORD_AUDIO).then(result => {
+      // Alert.alert(result);
+      console.log(result);
+    });
+  }, []);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  // test33@buzzni.com
+  // 431ffefc60f894c27a46d47d65c4dcb627c9c8c1
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView style={styles.container}>
+      <WebView
+        key={3}
+        style={styles.container}
+        // source={{uri: 'http://192.168.2.7:3000/chat?d_key=test'}}
+        // source={{uri: 'http://127.0.0.1:3000/chat?d_key=test'}}
+        source={{
+          uri: 'https://71e3-222-112-203-51.ngrok-free.app/chat?d_key=test',
+        }}
+        // originWhitelist={['*']}
+        // source={{uri: 'https://oksoon.buzzni.com/chat?d_key=test'}}
+        // useWebView2
+        // webviewDebuggingEnabled
+        // javaScriptCanOpenWindowsAutomatically
+        // mediaPlaybackRequiresUserAction
+        // remoteDebuggingEnabled
+        // allowsInlineMediaPlayback
+        // allowsAirPlayForMediaPlayback
+        // mediaCapturePermissionGrantType="grant"
+        // javaScriptEnabled
+        // userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15"
+        // userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
+        // mixedContentMode="always"
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
   },
 });
-
-export default App;
